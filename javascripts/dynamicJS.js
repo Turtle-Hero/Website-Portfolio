a7c33398f2f55e4307db2d4edff747502c3feadf
Trevor Warner <trevorwarner11@augustana.edu>
2015-06-30 12:53:38 -0500

    Update dynamicJS.js

--- javascripts/dynamicJS.js
+++ javascripts/dynamicJS.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
+// Variables for Work scrolling functionaltiy
 
 	var itemWidth = $('.workItem').width();		//width of work div
 	var doubleWidth = itemWidth + itemWidth;	// double width of work div
 	var current = 0;	//current horizontal position of work div
 	var index = 0;		//current position of circle indicators
 
+// Variables for Dashboard functionality
 
-	var scrollDist;
-	var marginOffset = 0.5 * parseFloat($('#about').css("margin-top"));
-	
-	var totOffset;
+	var scrollDist; 	//offset of div to traverse to
+	var marginOffset; 	//offset of margin inbetween div's
+	var dashHeight; 	// height of dashboard for smaller version of website
+	var totOffset;		//sum of everything needed to traverse to div
 
 	$('#aboutLink').click(function( e ){
-
 		e.preventDefault();
 
 		scrollDist = $("#about").offset().top;
+		marginOffset = 0.5 * parseFloat($('#about').css("margin-top"));
+		dashHeight = $('#dashboard').height();
+
 		totOffset = scrollDist - marginOffset;
+		if ($('#dashboard').css('z-index') == 100){
+			totOffset = totOffset - (dashHeight + 20);
+		}
 
 		$("html, body").animate( { scrollTop: totOffset}, 800);
 	
@@ -27,18 +34,29 @@ $(document).ready(function() {
 		e.preventDefault();
 
 		scrollDist = $("#work").offset().top;
+		marginOffset = 0.5 * parseFloat($('#about').css("margin-top"));
+		dashHeight = $('#dashboard').height();
+
 		totOffset = scrollDist - marginOffset;
+		if ($('#dashboard').css('z-index') == 100){
+			totOffset = totOffset - (dashHeight + 20);
+		}
 
 		$("html, body").animate( { scrollTop: totOffset}, 800);
 	});
 
 
 	$('#contactLink').click(function(e ){
-
 		e.preventDefault();
 
 		scrollDist = $("#contact").offset().top;
+		marginOffset = 0.5 * parseFloat($('#about').css("margin-top"));
+		dashHeight = $('#dashboard').height();
+
 		totOffset = scrollDist - marginOffset;
+		if ($('#dashboard').css('z-index') == 100){
+			totOffset = totOffset - (dashHeight + 20);
+		}
 
 		$("html, body").animate( { scrollTop: totOffset}, 800);
 
